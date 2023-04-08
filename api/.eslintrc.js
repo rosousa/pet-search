8dc69d3dcfd03549b92596cc2d0f10cc348cba6b
Rodolfo Sousa <rodolfozsousa@gmail.com>
2023-04-08 14:44:29 -0300

    fix: cannot read tsconfig

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['./tsconfig.json', './tsconfig.eslint.json'],
+    tsconfigRootDir: __dirname
   },
   overrides: [
     {
