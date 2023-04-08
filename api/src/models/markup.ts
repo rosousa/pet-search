779d45c655d6187c68a9574d8fab898924067cc1
Rodolfo Sousa <rodolfozsousa@gmail.com>
2023-04-08 15:15:14 -0300

    refactor: markup model types

--- api/src/models/markup.ts
+++ api/src/models/markup.ts
@@ -1,14 +1,20 @@
 class Markup {
-  id: string;
+  id: number;
 
   lat: number;
 
   lng: number;
 
-  constructor({ id, lat, lng }: Markup) {
+  createdAt: Date;
+
+  updatedAt: Date;
+
+  constructor({ id, lat, lng, createdAt, updatedAt }: Markup) {
     this.id = id;
     this.lat = lat;
     this.lng = lng;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
   }
 }
 
--- api/src/repositories/markup.repository.ts
+++ api/src/repositories/markup.repository.ts
@@ -1,7 +1,17 @@
 import prisma from '../config/prisma';
+import { Markup } from '../models';
 
 export class MarkupRepository {
-  async get() {
+  async get(): Promise<Markup[]> {
     return prisma.markup.findMany();
+  }
+
+  async create({ lat, lng }: Pick<Markup, 'lat' | 'lng'>) {
+    return prisma.markup.create({
+      data: {
+        lat,
+        lng,
+      },
+    });
   }
 }
