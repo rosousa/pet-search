96a6f9bc2fa08fd860d32399a9eefa6361b96109
Rodolfo Sousa <rodolfozsousa@gmail.com>
2023-04-10 21:34:24 -0300

    feat: add request for markups

--- front-end/src/components/Map/index.tsx
+++ front-end/src/components/Map/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 import MapEvents, { ILocation } from './MapEvents';
 import CustomModal from '../Modal';
@@ -10,10 +10,24 @@ import {
   StyledTitle,
   StyledDescription,
 } from './styles';
+import { get } from '../../api';
+
+type Markup = {
+  id: number;
+  lat: number;
+  lng: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
 function Map() {
   const [location, setLocation] = useState<ILocation>({ lat: 0, lng: 0 });
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [markups, setMarkups] = useState<Markup[]>([]);
+
+  useEffect(() => {
+    get('/api/markup').then((data) => setMarkups(data));
+  }, []);
 
   return (
     <Container>
@@ -29,24 +43,31 @@ function Map() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={{ lat: location.lat, lng: location.lng }}>
-            <StyledPopup>
-              <StyledImage
-                src="http://cdn.shopify.com/s/files/1/0086/0795/7054/articles/Cat_s_Mind_x630.jpg?v=1624444348"
-                alt="animal"
-              />
-              {/* refactor */}
-              <StyledTitle>
-                Thor <br />
-                <span>(00) 00000-0000</span>
-              </StyledTitle>
-              <StyledDescription>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Adipisci at quos aut modi non nesciunt explicabo impedit sit
-                iusto commodi, maiores maiores
-              </StyledDescription>
-            </StyledPopup>
-          </Marker>
+          {markups.map((markup) => {
+            return (
+              <Marker
+                key={markup.id}
+                position={{ lat: markup.lat, lng: markup.lng }}
+              >
+                <StyledPopup>
+                  <StyledImage
+                    src="http://cdn.shopify.com/s/files/1/0086/0795/7054/articles/Cat_s_Mind_x630.jpg?v=1624444348"
+                    alt="animal"
+                  />
+                  {/* refactor */}
+                  <StyledTitle>
+                    Thor <br />
+                    <span>(00) 00000-0000</span>
+                  </StyledTitle>
+                  <StyledDescription>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Adipisci at quos aut modi non nesciunt explicabo impedit sit
+                    iusto commodi, maiores maiores
+                  </StyledDescription>
+                </StyledPopup>
+              </Marker>
+            );
+          })}
         </MapContainer>
       </Wrapper>
     </Container>
