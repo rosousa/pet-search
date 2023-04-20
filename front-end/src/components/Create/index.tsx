2594b7af3773c082d4c4cc54083370a35c95a288
Rodolfo Sousa <rodolfozsousa@gmail.com>
2023-04-20 19:58:33 -0300

    feat: form layout

--- front-end/src/components/Create/index.tsx
+++ front-end/src/components/Create/index.tsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 
+type UserInput = {
+  tel: string;
+  description: string;
+};
+
 function CreateMarkup() {
+  const [userInput, setUserInput] = useState<UserInput>();
+
   const markup = useSelector((state: RootState) => state.markup);
 
   return (
@@ -10,7 +18,46 @@ function CreateMarkup() {
         markup.selectedLocation.selected ? '' : 'hidden'
       } flex flex-col items-center `}
     >
-      <div className="w-5/6 flex flex-col items-center border-2 border-zinc-700 bg-zinc-800 rounded pt-10 pb-10 text-white text-center"></div>
+      <div className="w-5/6 h-full flex flex-col items-center border-2 border-zinc-700 bg-zinc-800 rounded text-white text-center">
+        <form className="flex flex-col items-center gap-y-5 p-3">
+          <div className="flex flex-col gap-y-2">
+            <label htmlFor="" className="text-md font-bold">
+              Name
+            </label>
+            <input
+              type="text"
+              className="h-10 rounded text-xl bg-zinc-900 px-2 focus:outline-none"
+            />
+          </div>
+          <div className="flex flex-col gap-y-1">
+            <label htmlFor="" className="text-md font-bold">
+              Tel
+            </label>
+            <input
+              type="text"
+              className="h-10 rounded text-xl bg-zinc-900 px-2 focus:outline-none"
+            />
+          </div>
+          <div className="flex flex-col gap-y-1">
+            <label htmlFor="" className="text-md font-bold">
+              Image URL
+            </label>
+            <input
+              type="text"
+              className="h-10 rounded text-xl bg-zinc-900 px-2 focus:outline-none"
+            />
+          </div>
+          <div className="flex flex-col gap-y-1">
+            <label htmlFor="" className="text-md font-bold">
+              Description
+            </label>
+            <textarea className="h-40 rounded text-xl bg-zinc-900 p-2 resize-none focus:outline-none" />
+          </div>
+          <button className="bg-blue-600 w-40 h-10 rounded font-bold text-sm hover:brightness-75">
+            CREATE
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
